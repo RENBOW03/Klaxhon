73afc9d510a5a004110dc3c185bc386357569a40
RENBOW03 <thanapat16849@gmail.com>
2022-07-08 22:52:02 +0700

    My first terminal 7/8/22

---
+++ kids-styles/J.js
@@ -0,0 +1,20 @@
+function slider(){
+  $current = $('.slider img.active');
+  if($current.length==0)
+  {
+    $('slider img:first-child').addClass('active');
+  }
+  else
+  {
+    $next = $current.removeClass('active').next();
+    if($next.length==0)
+    {
+      $('.slider img:first-child').addClass('active');
+    }
+    else
+    {
+      $next.addClass('active');
+    }
+  }
+}
+setInterval(slider,3000);
